1daf1add719d4d911084e7501518a4ae68af7d70
pineblue <pinebluesoft@gmail.com>
2025-07-26 07:28:49 -0400

    init

---
+++ src/main/java/com/schema/registry/glue_schema_registry/GlueSchemaRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.schema.registry.glue_schema_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlueSchemaRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlueSchemaRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schema/registry/glue_schema_registry/GlueSchemaRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schema.registry.glue_schema_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlueSchemaRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
